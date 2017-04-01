55d5febd60e6b2f74fa1e598a6a097fc113b7333
hcxowe <hcxowe@126.com>
2017-04-01 20:35:08 +0800

    webpack-tutorial

---
+++ webpack-tutorial/tutorial-02/webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    entry: './app/index.js',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            //sourceMap: options.devtool && (options.devtool.indexOf('sourcemap') >=0 || options.devtool.indexOf('source-map') >= 0)
+        })
+    ]
+};
