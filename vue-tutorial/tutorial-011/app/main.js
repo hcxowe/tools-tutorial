e96ec78d47b444e6f1981f9c04ac19cf937d54a4
hcxowe <hcxowe@126.com>
2017-04-14 01:03:34 +0800

    过渡效果

--- vue-tutorial/tutorial-011/app/main.js
+++ vue-tutorial/tutorial-011/app/main.js
@@ -1,5 +1,6 @@
 // 过渡效果
 var Vue = require('vue');
+var _ = require('lodash');
 
 var app = new Vue({
     el: '#app',
@@ -27,3 +28,93 @@ var app = new Vue({
         }            
     }
 });
+
+var list = new Vue({
+    el: '#list-demo',
+    data: {
+        items: [1,2,3,4,5,6,7,8,9],
+        nextNum: 10
+    },
+    methods: {
+        randomIndex: function() {
+            return Math.floor(Math.random() * this.items.length);
+        },
+        add: function() {
+            this.items.splice(this.randomIndex(), 0, this.nextNum++);
+        },
+        remove: function() {
+            this.items.splice(this.randomIndex(), 1);
+        }
+    }
+});
+
+var flip = new Vue({
+    el: '#flip-list-demo',
+    data: {
+        items: [1,2,3,4,5,6,7,8,9]
+    },
+    methods: {
+        shuffle: function () {
+            this.items = _.shuffle(this.items)
+        }
+    }
+});
+
+var list2 = new Vue({
+    el: '#list-complete-demo',
+    data: {
+        items: [1,2,3,4,5,6,7,8,9],
+        nextNum: 10
+    },
+    methods: {
+        randomIndex: function () {
+            return Math.floor(Math.random() * this.items.length)
+        },
+        add: function () {
+            this.items.splice(this.randomIndex(), 0, this.nextNum++)
+        },
+        remove: function () {
+            this.items.splice(this.randomIndex(), 1)
+        },
+        shuffle: function () {
+            this.items = _.shuffle(this.items)
+        }
+    }
+});
+
+var list3 = new Vue({
+    el: '#list-more-demo',
+    data: {
+        //items: [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,]
+        items: Array.apply(null, { length: 81 })
+            .map(function (_, index) { 
+            return {
+                id: index,
+                number: index % 9 + 1
+            }
+        })
+    },
+    methods: {
+        shuffle: function() {
+            this.items = _.shuffle(this.items);
+        }
+    }
+});
+
+new Vue({
+    el: '#sudoku-demo',
+    data: {
+        cells: Array.apply(null, { length: 81 })
+            .map(function (_, index) { 
+            return {
+                id: index,
+                number: index % 9 + 1
+            }
+        })
+    },
+    methods: {
+        shuffle: function () {
+            this.cells = _.shuffle(this.cells)
+        }
+    }
+})
