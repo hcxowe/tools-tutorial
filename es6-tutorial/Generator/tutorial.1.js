3b5c2e56c3e3822af0793ca5fc062b36ea557be4
hcxowe <hcxowe@126.com>
2017-05-26 09:59:50 +0800

    finish generator

--- es6-tutorial/Generator/tutorial.1.js
+++ es6-tutorial/Generator/tutorial.1.js
@@ -67,6 +67,40 @@ for (var v of something) {
 
 console.log(something.next().value); // 321
 
+//************************************************************************************************
+
+// ES6最完美的世界就是生成器（看似同步的异步代码）和Promise（可信任可组合）的结合
+
+// 执行一个生成器，把上一个yield的值传递给下一个yield
+function run(gen) {
+    var args = [].slice.call(arguments, 1), it;
+
+    // 初始化生成器
+    it = gen.apply(this, args);
+
+    return Promise.resolve().then(function handleNext(value) {
+        // 上一个yield使用value代替， 并执行到下一个yield，并返回yield返回的值
+        var next = it.next(value);
+
+        return (function handleResult(next) {
+            // 如果生成器执行完成，则返回生成器最后返回的值
+            if (next.done) {
+                return next.value;
+            }
+            // 继续下一个yield
+            else {
+                return Promise.resolve(next.value).then(
+                    handleNext, // 向生成器传回上一步生成器得到的值
+
+                    // reject时，把错误传回生成器进行错误处理
+                    function handleErr(err) {
+                        return Promise.resolve(it.throw(err)).then(handleResult)
+                    })
+            }
+
+        }(next));
+    });
+}
 
 
 
