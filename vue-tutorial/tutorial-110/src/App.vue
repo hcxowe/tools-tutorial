1b9bc54872e60d01b7e05dc054794725c4c73445
hcxowe <hcxowe@126.com>
2017-09-04 08:39:31 +0800

    init order system

---
+++ vue-tutorial/tutorial-110/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<div id="app">
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'app'
+	}
+</script>
+
+<style>
+	* {
+		box-sizing: border-box;
+	}
+
+	html, body {
+		width: 100%;
+		height: 100%;
+		font-family: '微软雅黑', sans-serif;
+		overflow: hidden;
+	}
+
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		text-align: center;
+		color: #2c3e50;
+		width: 100%;
+		height: 100%;
+	}
+</style>
