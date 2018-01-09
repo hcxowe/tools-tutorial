e53063965d7c7278a27478fe9470f26fe54147f4
hcxowe <hcxowe@126.com>
2018-01-09 16:47:05 +0800

    jQuery日历插件

---
+++ EasyUI/jquery-xcalendar/jquery-xcalendar.js
@@ -0,0 +1,19 @@
+;(function($, window, undefined){
+    var pluginName = 'xCalendar',
+        defaults = {
+
+        }
+
+    var XCalendar = function(el, opts) {
+        this.$el = el
+        this.options = $.extend({}, defaults, opts)
+    }
+
+    XCalendar.prototype = {
+
+    }
+
+    $.fn.xCalendar = function(opts) {
+        
+    }
+}(jQuery, window))
