34b39438d9d081ed89e6ac2cdb4832ca047324b6
hcxowe <hcxowe@126.com>
2018-01-10 17:14:21 +0800

    日历日历 头部逻辑

---
+++ EasyUI/jquery-xcalendar/data.js
@@ -0,0 +1,32 @@
+var data = [
+    {
+        year: '2017',
+        month: [
+            {
+                month: 1,
+                day: [
+                    {
+                        day: 1,
+                        time: 22.5,
+                        desc: 'desc'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        year: '2018',
+        month: [
+            {
+                month: 1,
+                day: [
+                    {
+                        day: 1,
+                        time: 22.5,
+                        desc: 'desc'
+                    }
+                ]
+            }
+        ]
+    }
+];
