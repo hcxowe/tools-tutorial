e5090c6c38d0fa6fa658682f41dd5c8aaf0a4cce
hcxowe <hcxowe@126.com>
2018-05-10 17:36:13 +0800

    XJXD 首页图片

--- EasyUI/server.js
+++ EasyUI/server.js
@@ -1,6 +1,19 @@
 var express = require("express");
+const proxy = require('http-proxy-middleware');
 var app = express();
 
-app.use("/",express.static(__dirname + ""));
+app.use("/", express.static(__dirname + ""));
 
-app.listen(3000);
+const apiProxy = proxy('/', { 
+    target: 'http://192.168.0.85:9000',
+    changeOrigin: true,
+    onProxyReq(proxyReq, req, res) {
+
+    }
+});
+
+app.use('/', apiProxy)
+
+app.listen(3000, () => {
+    console.log('Listening on: http://localhost:3000');
+});
