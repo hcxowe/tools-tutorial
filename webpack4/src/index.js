5455df34e5a5708c1e916e426b3067bc312df1b7
hcxowe <hcxowe@126.com>
2018-05-23 17:41:11 +0800

    webpack4 起步

---
+++ webpack4/src/index.js
@@ -0,0 +1,17 @@
+import _ from 'lodash'
+import './style.css'
+import Icon from './1.png'
+
+function component() {
+    let element = document.createElement('div')
+    element.innerHTML = _.join(['hello', 'webpack'], ' ')
+    element.classList.add('hello')
+
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
+    return element
+}
+
+document.body.appendChild(component())
---
+++ webpack4/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            /* {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                enforce: 'post',
+                loader: 'es3ify-loader'
+            }, */
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        
+    ]
+}
